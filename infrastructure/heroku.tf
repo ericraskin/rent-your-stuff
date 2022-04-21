56131c8a8a2a73f6d50e69862281101f98a21168
Eric Raskin <eraskin@paslists.com>
2022-04-21 17:05:09 -0400

    create heroku staging app - db and GH integration

--- infrastructure/heroku.tf
+++ infrastructure/heroku.tf
@@ -7,3 +7,32 @@ resource "heroku_app" "eraskin-rys-staging" {
   ]
 
 }
+
+resource "heroku_addon" "eraskin-rys-staging-db" {
+  name = heroku_app.eraskin-rys-staging.id
+  plan = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_pipeline" "eraskin-rys-pipeline" {
+  name = "eraskin-rys-pipeline"
+}
+
+resource "heroku_pipeline_coupling" "staging_pipeline_coupling" {
+  app = heroku_app.eraskin-rys-staging.id
+  pipeline = heroku_pipeline.eraskin-rys-pipeline.id
+  stage = "staging"
+}
+
+resource "herokux_pipeline_github_integration" "pipeline_integration" {
+  pipeline_id = heroku_pipeline.eraskin-rys-pipeline.id
+  org_repo = "ericraskin/eraskin-rent-your-stuff"
+}
+
+resource "herokux_app_github_integration" "eraskin-rys-gh-integration" {
+  app_id = heroku_app.eraskin-rys-staging.uuid
+  branch = "main"
+  auto_deploy = true
+  wait_for_ci = true
+
+  depends_on = [herokux_pipeline_github_integration.pipeline_integration]
+}
