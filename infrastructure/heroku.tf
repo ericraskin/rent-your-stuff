0166999065e77b7924201a357d8d740967843546
Eric Raskin <eraskin@paslists.com>
2022-04-21 16:39:38 -0400

    create heroku staging app - fix typos

--- infrastructure/heroku.tf
+++ infrastructure/heroku.tf
@@ -2,12 +2,8 @@ resource "heroku_app" "eraskin-rent-your-stuff-staging" {
   name = "eraskin-rent-your-stuff-staging"
   region = "us"
 
-  config_vars = {
-    FOOBAR = "baz"
-  }
-
-  buildpacks = {
+  buildpacks = [
     "heroku/gradle"
-  }
+  ]
 
 }
